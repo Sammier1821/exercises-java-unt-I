ee5244e77632fb20ae98bfc899610a540619edde
Sammier1821 <productive.gian.112020@gmail.com>
2022-09-12 13:48:45 -0500

    the initial project

---
+++ src/EJERCICIO01.java
@@ -0,0 +1,5 @@
+public class EJERCICIO01 {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
---
+++ src/EJERCICIO02.java
@@ -0,0 +1,5 @@
+public class EJERCICIO02 {
+  public static void main(String[] args) {
+    System.out.println("Hello2");
+  }
+}
