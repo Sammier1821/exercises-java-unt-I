78c2bfa4b697df105d8e4029d63e6598f7bee942
Sammier1821 <productive.gian.112020@gmail.com>
2022-09-16 15:18:15 -0500

    Classes update 16/09/22

---
+++ src/ControlClasses/Control.java
@@ -0,0 +1,17 @@
+package ControlClasses;
+
+import java.util.Scanner;
+
+public class Control {
+  public static boolean control() {
+    Scanner sc = new Scanner(System.in);
+    char resp = ' ';
+    do {
+      System.out.print("Do you want to restart the program? (y/n): ");
+      resp = sc.next().charAt(0);
+    } while (resp != 'y' && resp != 'Y' && resp != 'n' && resp != 'N');
+    if (resp == 'y' || resp == 'Y')
+      return true;
+    return false;
+  }
+}
---
+++ src/MathClasses/Constants.java
@@ -0,0 +1,6 @@
+package MathClasses;
+
+public class Constants {
+  public static final float PI = 3.14f;
+  public static final float G = 9.8f;
+}
