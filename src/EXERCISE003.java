cf3f9a4d08115a4d3643baf7733f8bae27e4999a
Sammier1821 <productive.gian.112020@gmail.com>
2022-09-16 15:16:48 -0500

    Exercise003 v1.0

---
+++ src/EXERCISE003.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import ControlClasses.Control;
+import MathClasses.Constants;
+
+/* Instructions:
+ * 1. Use a cilinder volume formula
+ * 2. Input a height (in meters)
+ * 3. Calculate volume
+ */
+
+public class EXERCISE003 {
+  public static float getCilinderVolume(float radius, float height) {
+    return Constants.PI * (float) (Math.pow(radius, 2)) * height;
+  }
+  public static void report(float x) {
+    System.out.println("The volume of the cilinder is " + x);
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float radius = 2f, height = 0;
+    do {
+      System.out.print("Height value: ");
+      height = sc.nextFloat();
+      report(getCilinderVolume(radius, height));
+    } while (Control.control());
+  }
+}
