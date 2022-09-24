d2a98061bc9bcea1322908c5d4e5731348fea204
Sammier1821 <productive.gian.112020@gmail.com>
2022-09-24 12:38:25 -0500

    Exercise010 v1.0

---
+++ src/EXERCISE010.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+import ControlClasses.Control;
+
+/* Instructions:
+ * 1. Calculate interest from a capital, an interest rate (expressed 
+ * in percentage) and a number of periods
+ * Data:
+ * - Amount = Capital * (1 + rate/100)^Period Count
+ * - Interest = Amount - Capital
+ */
+
+public class EXERCISE010 {
+  public static Scanner sc = new Scanner(System.in);
+  
+  public static void main(String[] args) {
+    float capital, interestRate, interest;
+    int nYears;
+    do {
+      // INPUT
+      do {
+        System.out.print("Enter a capital: ");
+        capital = sc.nextFloat();
+      } while (capital <= 0);
+      do {
+        System.out.print("Enter the interest rate (where 1 equals 1%): ");
+        interestRate = sc.nextFloat();
+      } while (interestRate <= 0);
+      do {
+        System.out.print("Enter a number of periods (year/s): ");
+        nYears = sc.nextInt();
+      } while (nYears <= 0);
+      // PROCESS
+      interest = capital * (float) Math.pow((1 + interestRate / 100), nYears) - capital;
+      // OUTPUT
+      System.out.println("The generated interest is: " + interest);
+    } while (Control.control());
+  }  
+}
